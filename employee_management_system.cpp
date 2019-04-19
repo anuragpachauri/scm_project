4810a950fe552de4185cdc07a546906949eba037
anuragpachauri <anuragpachauri8@gmail.com>
2019-04-19 15:11:31 +0530

    int main

--- employee_management_system.cpp
+++ employee_management_system.cpp
@@ -42,4 +42,89 @@ int num;
 
 employee emp[max],tempemp[max],sortemp[max],sortemp1[max];
 
+int main()
+
+{
+    
+    system("cls");
+    
+    void build();
+    
+    void list();
+    
+    void insert();
+    
+    void deletes();
+    
+    void edit();
+    
+    void search();
+    
+    void sort();
+    
+    char option;
+    
+    void menu();
+    
+    menu();
+    
+    while((option=cin.get())!='q')
+        
+    {
+        
+        switch(option)
+        
+        {
+                
+            case 'b':
+                
+                build();
+                
+                break;
+                
+            case 'l':
+                
+                list();
+                
+                break;
+                
+            case 'i':
+                
+                insert();
+                
+                break;
+                
+            case 'd':
+                
+                deletes();
+                
+                break;
+                
+            case 'e':
+                
+                edit();
+                
+                break;
+                
+            case 's':
+                
+                search();
+                
+                break;
+                
+            case 'n':
+                
+                sort();
+                
+                break;
+                
+        }
+        
+        menu();
+        
+    }
+    
+    return 0;
+    
+}
 
