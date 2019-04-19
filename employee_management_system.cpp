b976bd7555c01cf8555ac47877627798d0f56ebe
anuragpachauri <anuragpachauri8@gmail.com>
2019-04-19 15:08:04 +0530

    header files

---
+++ employee_management_system.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+#include<cstring>
+
+#include<cstdlib>
+
+#include<iomanip>
+
+#include<windows,h>
+
+//#include <ctime>
+
+//#include <dos.h>
+
+#include<dos.h>
+
+#include<conio.h>
+
+#include<cstdio>
+
+#define max 20
+
+using namespace std;
+
